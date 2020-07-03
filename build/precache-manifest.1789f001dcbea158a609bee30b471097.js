21ae87643fc7470bd8f51d457d7047209095e6df
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2020-07-03 02:08:35 +0100

    Added my project

---
+++ build/precache-manifest.1789f001dcbea158a609bee30b471097.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfac2fc946616c781666d57569bed514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b661c3d22e93ff110b83",
+    "url": "/static/css/main.2eadbc01.chunk.css"
+  },
+  {
+    "revision": "c5275fd7867a0e5496c8",
+    "url": "/static/js/2.47be75d2.chunk.js"
+  },
+  {
+    "revision": "7d6b40073579b814286a6a4cc2e947db",
+    "url": "/static/js/2.47be75d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b661c3d22e93ff110b83",
+    "url": "/static/js/main.40daad34.chunk.js"
+  },
+  {
+    "revision": "5e1354b178bc063acab5",
+    "url": "/static/js/runtime-main.08cfc7ed.js"
+  },
+  {
+    "revision": "ab0e85ee2de98383ba1da90387bc6614",
+    "url": "/static/media/backg.ab0e85ee.svg"
+  },
+  {
+    "revision": "3242db76f2d2b3e2c740353fcafb6349",
+    "url": "/static/media/background.3242db76.jpg"
+  },
+  {
+    "revision": "7a2a216b0fae3ad34394ec7e938ae50d",
+    "url": "/static/media/box11.7a2a216b.png"
+  },
+  {
+    "revision": "ba19b816bc814b803800ad434d947bcf",
+    "url": "/static/media/logb.ba19b816.svg"
+  },
+  {
+    "revision": "a7070a62a67624ce787f4ee802b44391",
+    "url": "/static/media/pos-bg.a7070a62.png"
+  }
+]);
